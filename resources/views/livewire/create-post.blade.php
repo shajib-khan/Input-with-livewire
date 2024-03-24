7be411b6c3f2f83e02b86fef159a0b9d473c3d36
shajib-khan <shajibkhan946@gmail.com>
2024-03-24 09:13:43 +0600

    button design

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -11,9 +11,22 @@
         <button class="btn btn-primary">Submit</button>
       </form>
 
-    @foreach ($posts as $post)
-    <p>{{ $post->name }}</p>
-    <a href=""button class="btn btn-danger">delete</button>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Text</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+            <tr></tr>
+        <td>{{ $post->name }}</td>
+        <td> <button type="button" class="btn btn-danger">Delete</button></td>
+        <td> <button type="button" class="btn btn-primary">Edit</button></td>
+        </tr>
+        @endforeach
 
-@endforeach
+
+        </tbody>
+      </table>
 </div>
